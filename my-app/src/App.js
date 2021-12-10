a330086cf6db69b281d5c1f7e9197281bd964384
IvanJunoy <ivanjunoy@gmail.com>
2021-12-10 00:14:12 -0300

    0.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; Apartir de v6 Switch se remplazo
+import Header from "./components/Header";
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola!</h1>
+      <Router>
+        <Header />
+        {/* <Switch> */}
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" exact element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+        {/* </Switch> */}
+      </Router>
     </div>
   );
 }
---
+++ my-app/src/components/ProductListing.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+import { setProducts } from "../redux/actions/productActions"
+
+const ProductListing = () => {
+    const products = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const fetchProducts = async () => {
+        const response = await axios
+            .get("https://fakestoreapi.com/products")
+            .catch((err) => {
+                console.log("Err", err);
+            });
+        dispatch(setProducts(response.data));
+    };
+
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    console.log("Products: ", products);
+    return (
+        <div className="ui grid">
+            {/* <h1> */}
+            <ProductComponent />
+            {/* </h1> */}
+        </div>
+    )
+}
+
+export default ProductListing;
